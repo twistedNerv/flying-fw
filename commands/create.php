86a15df3b0d52b406a9144b845adc716dfebfd43
twistedNerv <sebastjan@gmail.com>
2019-12-27 00:20:46 +0100

    template helper idea, create update start

--- commands/create.php
+++ commands/create.php
@@ -4,7 +4,7 @@ require_once '../app/core/db.php';
 require_once '../app/config/globals.php';
 
 class createModel extends db{
-    
+   
     public $db;
     public $name;
     public $tableCheck = 0;
@@ -13,13 +13,13 @@ class createModel extends db{
     public $viewCheck = 0;
     public $modelCheck = 0;
     public $columns = [];
-    
+   
     public function __construct($argv) {
         parent::__construct();
         $this->db = new db();
         $this->run($argv);
     }
-    
+   
     private function run($argv) {
         $this->checkCommand($argv);
         $this->tableCheck = ($this->tableCheck) ? $this->tableExist() : 0;
@@ -27,7 +27,7 @@ class createModel extends db{
         $this->modelCheck = ($this->modelCheck) ? ((file_exists("../app/models/" . $this->name . "Model.php")) ? 1 : 0) : 1;
         $this->controllerCheck = ($this->controllerCheck) ? ((file_exists("../app/controllers/" . $this->name . "Controller.php")) ? 1 : 0) : 1;
         $this->viewCheck = ($this->viewCheck) ? ((file_exists("../app/views/default/" . $this->name . "/index.php")) ? 1 : 0) : 1;
-        
+       
         if($this->tableCheck) {
             echo "Create table $this->name not selected or table already exist. \n\r";
             if(!$this->sqlDumpCheck) {
@@ -45,7 +45,7 @@ class createModel extends db{
                 //return false;
             }
         }
-        
+       
         if($this->modelCheck || $this->tableExist() == 0) {
             echo "Create model $this->name not selected, model alerady exist or table doesn't exist.\n\r";
         } else {
@@ -53,14 +53,15 @@ class createModel extends db{
             $this->createModel();
             echo "Model $this->name.php created.\n\r";
         }
-        
+       
         if($this->controllerCheck) {
             echo "Create controller $this->name not selected or controller alerady exist.\n\r";
         } else {
+            $this->columns = $this->db->getTableColumns($this->name);
             $this->createController();
             echo "Controller $this->name.php created.\n\r";
         }
-        
+       
         if($this->viewCheck) {
             echo "View $this->name alerady exist.\n\r";
         } else {
@@ -68,7 +69,7 @@ class createModel extends db{
             echo "View view/$this->name/index.php created.\n\r";
         }
     }
-    
+   
     private function checkCommand($argv) {
         if(!$argv[1]) {
             echo "Error: No package name selected.\nUse '[command] --help' for help.";
@@ -91,7 +92,7 @@ class createModel extends db{
         if(preg_match('/^[a-zA-Z0-9$_]+$/', $argv[1]) != 1) {
             echo "\nError: Forbidden characters used for package name.\nUse '[command] --help' for help.\n";
             die;
-        } 
+        }
         if(!$argv[2]) {
             echo "Error: No option selected.\nUse '[command] --help' for help.";
             die;
@@ -124,7 +125,7 @@ class createModel extends db{
             }
         }
     }
-    
+   
     private function createController() {
         $fileString = "<?php";
         $fileString .=  "\n\n";
@@ -134,33 +135,70 @@ class createModel extends db{
         $fileString .= "\n";
         $fileString .= "\t\tparent::__construct();";
         $fileString .= "\n\t}";
-        $fileString .= "\n\n";
-        $fileString .= "\tpublic function index() {";
+        $fileString .= "\n\n\t";
+        $fileString .= 'public function updateAction($id=0) {';
         $fileString .= "\n";
         $fileString .= "\t\t";
-        $fileString .= '/*$' . $this->name . 'Model = $this->loadModel("' . $this->name . '");';
+        $fileString .= '$' . $this->name . 'Model = $this->loadModel("' . $this->name . '");';
         $fileString .= "\n";
         $fileString .= "\t\t";
-        $fileString .= '$' . $this->name . 'Model->findOneById(1);';
+        $fileString .= 'if($id != 0) {';
+        $fileString .= "\n";
+        $fileString .= "\t\t\t";
+        $fileString .= '$' . $this->name . 'Model->findOneById($id);';
         $fileString .= "\n";
         $fileString .= "\t\t";
-        $fileString .= '$' . $this->name . 'Model->name = "test";';
+        $fileString .= "}";
         $fileString .= "\n";
         $fileString .= "\t\t";
+        $fileString .= 'if(isset($_POST["action"]) && $_POST["action"] == "handle' . $this->name . '") {';
+        foreach($this->columns as $singleColumn) {
+            $fileString .= "\n\t\t\t";
+            $fileString .= '$' . $this->name . 'Model->set' . ucfirst($singleColumn) . '(filter_var($_POST["' . $this->name . '-' . $singleColumn . '"], FILTER_SANITIZE_STRING));';
+        }
+        $fileString .= "\n\t\t\t";
         $fileString .= '$' . $this->name . 'Model->flush();';
-        $fileString .= "\n";
-        $fileString .= "\t\t";
+        $fileString .= "\n\t\t\t";
+        $fileString .= '$action = ($id != 0) ? "' . ucfirst($this->name) . ' element with id: $id updated successfully." : "' . $this->name . ' successfully added.";';
+        $fileString .= "\n\t\t\t";
+        $fileString .= '$this->tools->notification("' . $this->name . ' element dodan/urejen.", "primary");';
+        $fileString .= "\n\t\t\t";
+        $fileString .= '$this->tools->log("' . $this->name . '", $action);';
+        $fileString .= "\n\t\t";
+        $fileString .= "}";
+        $fileString .= "\n\t\t";
+        $fileString .= '$allItems = $' . $this->name . 'Model->findAll();';
+        $fileString .= "\n\t\t";
+        $fileString .= '$this->view->render("' . $this->name . '/update", ["items" => $allItems, "selected' . ucfirst($this->name) . '" => $' . $this->name . 'Model]);';
+        $fileString .= "\n\t";
+        $fileString .= '}';
+        $fileString .= "\n\n\t";
+        $fileString .= 'public function removeAction($id) {';
+        $fileString .= "\n\t\t";
+        $fileString .= 'if ($id) {';
+        $fileString .= "\n\t\t\t";
+        $fileString .= '$' . $this->name . 'Model = $this->loadModel("' . $this->name . '");';
+        $fileString .= "\n\t\t\t";
+        $fileString .= '$' . $this->name . 'Model->findOneById($id);';
+        $fileString .= "\n\t\t\t";
         $fileString .= '$' . $this->name . 'Model->remove();';
+        $fileString .= "\n\t\t\t";
+        $fileString .= '$this->tools->log("' . $this->name . '", "' . ucfirst($this->name) . ' element with id: $id removed.");';
+        $fileString .= "\n\t\t\t";
+        $fileString .= '$this->tools->redirect(URL . "' . $this->name . '/update");';
+        $fileString .= "\n\t\t";
+        $fileString .= '} else {';
+        $fileString .= "\n\t\t\t";
+        $fileString .= 'echo "No ' . $this->name . ' element id selected!";';
+        $fileString .= "\n\t\t";
+        $fileString .= '}';
+        $fileString .= "\n\t";
+        $fileString .= '}';
         $fileString .= "\n";
-        $fileString .= "\t\t";
-        $fileString .= '$' . $this->name . 'Model->flush();';
-        $fileString .= "\n";
-        $fileString .= "\t\t$" . "this->view->render('" . $this->name . "/index', ['name' => $" . $this->name . "Model->name]);*/";
-        $fileString .= "\n\t}";
-        $fileString .= "\n\n}";
+        $fileString .= '}';
         file_put_contents("../app/controllers/" . $this->name . "Controller.php", $fileString);
     }
-    
+   
     private function createModel() {
         $fileString = "<?php";
         $fileString .=  "\n\n";
@@ -211,9 +249,26 @@ class createModel extends db{
         $fileString .= "\n}";
         file_put_contents("../app/models/" . $this->name . "Model.php", $fileString);
     }
-    
+   
     private function createView() {
-        $fileString = "<h3>This is autogenerated text.</h3>\n";
+        $fileString = '<div class="col-sm-12 text-center">';
+        $fileString .= "\n\t";
+        $fileString .= '<h2>Nastavitve - ' . ucfirst($this->name) . '</h2>';
+        $fileString .= "\n\t";
+        $fileString .= '</div>';
+        $fileString .= "\n";
+        $fileString .= '<div class="col-sm-4 text-right">';
+        $fileString .= "\n\t";
+        $fileString .= '<div class="row">';
+        $fileString .= "\n\t\t";
+        $fileString .= '<div class="col-sm-12">';
+        $fileString .= "\n\t\t\t";
+        $fileString .= '<h4>' . ucfirst($this->name) . '</h4>';
+        $fileString .= "\n\t\t";
+        $fileString .= '</div>';
+       
+       
+       
         $fileString .= "<br>\n";
         $fileString .= 'Data from controller: <?=$data["id"]?>';
         $structure = '../app/views/default/' . $this->name . "/";
@@ -223,23 +278,23 @@ class createModel extends db{
             file_put_contents("../app/views/default/" . $this->name . "/index.php", $fileString);
         }
     }
-    
+   
     private function createTable() {
         $sql = file_get_contents("../app/dbschemas/" . $this->name . ".sql");
         $this->db->execute($sql);
     }
-    
+   
     private function createSqlDump() {
         $sql = "SHOW CREATE TABLE " . $this->name . ";";
         $result = $this->selectResult($sql);
         file_put_contents("../app/dbschemas/" . $this->name . ".sql", $result['Create Table']);
     }
-    
+   
     private function tableExist() {
         $sql = "DESCRIBE " . $this->name . ";";
         return ($this->selectResult($sql)) ? 1 : 0;
     }
- } 
+ }
 
 $newModel = new createModel($argv);
 //echo $newModel->name;
