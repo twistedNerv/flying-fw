ad8d4b223767d95f8208101f16d45c68b64f1259
twistedNerv <sebastjan@gmail.com>
2020-05-28 16:25:28 +0200

    globals

--- app/config/globals.php
+++ app/config/globals.php
@@ -4,6 +4,6 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_DATABASE', 'mvc');
 
-define('URL','http://localhost:8080/mvc/');
+define('URL','http://localhost/mvc/');
 define('APP_NAME', 'mvc');
 if(php_sapi_name() != 'cli') define('USER_IP', $_SERVER['REMOTE_ADDR']);
